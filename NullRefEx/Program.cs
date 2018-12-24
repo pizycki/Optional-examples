bfd9dec545bb26a7907a69c8360431239198bb9a
Izycki <Pawel.Izycki@BritishCouncil.Org>
2018-12-24 21:40:08 +0100

    Add project files.

---
+++ NullRefEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NullRefEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
